5455e414712941e30725cf7ee8dda81254c51007
unknown <Erlangga Achmad@.(none)>
2014-06-04 21:13:16 -0700

    Pemro01RegulerGenap13-14

---
+++ pemograman1-teori8-ErlanggaAchmad/src/Diskon.java
@@ -0,0 +1,12 @@
+// Created By ErlanggaAchmad
+// Update : 19 Mei 2014
+
+// Perintah Object Oriented Programming
+package praktikum08a.bin;
+import java.math.BigDecimal;
+
+public interface Diskon
+{
+	//public BigDecimal hitung();
+	public BigDecimal hitung(Transaksi t);
+}
