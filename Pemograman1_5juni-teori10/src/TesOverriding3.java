12947570a692a7fee26b88cb9365ae57d07c3fa7
unknown <Erlangga Achmad@.(none)>
2014-06-04 21:26:17 -0700

    Pemro01RegulerGenap13-14

---
+++ Pemograman1_5juni-teori10/src/TesOverriding3.java
@@ -0,0 +1,12 @@
+package Praktikum10c.bin;
+
+public class TesOverriding3
+{
+	//Membuat objek kelas komunitasPT dan memanggil fungsi turunan dan fungsi lokal
+	public static void main(String[] ErlanggaAchmad)
+	{
+		Karyawan K = new Karyawan("Sri Rezeki Candra Nursari", "Depok", "Wanita", 318, 2000000);
+		K.Tampilkan1();
+		K.Tampilkan2();
+	}
+}
